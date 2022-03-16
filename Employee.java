ac3c31e02a90d9546a80bc36577b44aee1d3f52d
Ashu0411 <101565216+Ashu0411@users.noreply.github.com>
2022-03-16 14:50:18 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,21 @@
+package basiccommands;
+import java.util.Scanner;
+public class Employee {
+
+	public static void main(String[] args) 
+	{
+		String name;
+		int age;
+		int salary;
+		Scanner e = new Scanner(System.in);
+		System.out.println("Name of the employee");
+		name =e.next();
+		System.out.println("Age of the employee");
+		age = e.nextInt();
+		System.out.println("Salary of the employee");
+		salary = e.nextInt();
+	}
+	{
+		
+	}
+}
