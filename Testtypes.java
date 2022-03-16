9a7e0fd20c05090a356992e260995d50ea12a0bf
Ashu0411 <101565216+Ashu0411@users.noreply.github.com>
2022-03-16 14:53:21 +0530

    Add files via upload

---
+++ Testtypes.java
@@ -0,0 +1,24 @@
+package basiccommands;
+
+public class Testtypes 
+{
+	  boolean b;
+	  int a;
+	  float c=90;
+	  double d;
+		public static void main(String[] args) 
+		{ 
+		  float r = 89.8f; 
+		  
+			
+		 Testtypes t= new Testtypes();
+		 System.out.println(t.b);
+		 System.out.println(t.a);
+		 System.out.println(t.c);
+		 System.out.println(t.d);
+		 
+		 
+		}
+
+	}
+
